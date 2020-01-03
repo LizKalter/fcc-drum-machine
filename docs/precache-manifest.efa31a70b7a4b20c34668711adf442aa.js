c2aed1f63529413d45f7ed466239c28e9f05876d
Liz Kalter <43416298+LizKalter@users.noreply.github.com>
2020-01-02 23:55:42 -0500

    updating with latest

---
+++ docs/precache-manifest.efa31a70b7a4b20c34668711adf442aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60c7a07be518c90b6fcd7bbdd91f7cf6",
+    "url": "/fcc-drum-machine/index.html"
+  },
+  {
+    "revision": "0d6950ce62a4c7e2b42c",
+    "url": "/fcc-drum-machine/static/css/main.a78a2f27.chunk.css"
+  },
+  {
+    "revision": "399937e2d4c431d6b34f",
+    "url": "/fcc-drum-machine/static/js/2.50fe4e04.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/fcc-drum-machine/static/js/2.50fe4e04.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0d6950ce62a4c7e2b42c",
+    "url": "/fcc-drum-machine/static/js/main.28265153.chunk.js"
+  },
+  {
+    "revision": "36d40ca10cf31a981ae7",
+    "url": "/fcc-drum-machine/static/js/runtime-main.7933606c.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,136 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Display(props) {
+	return (
+		<div id="display">{props.drumName}</div>
+	);
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+/* Sounds samples and names are from the freeCodeCamp example for this project
+   as I know nothing about drum samples.
+   https://codepen.io/freeCodeCamp/pen/MJyNMd
+*/
+function DrumMachine(props) {
+	const [currentDrum, setCurrentDrum] = useState('');
+
+	const Q = useRef(null);
+	const W = useRef(null);
+	const E = useRef(null);
+	const A = useRef(null);
+	const S = useRef(null);
+	const D = useRef(null);
+	const Z = useRef(null);
+	const X = useRef(null);
+	const C = useRef(null);
+
+	const drumMap = {
+		KeyQ: {
+			audio: Q,
+			name: 'Heater 1'
+		},
+		KeyW: {
+			audio: W,
+			name: 'Heater 2'
+		},
+		KeyE: {
+			audio: E,
+			name: 'Heater 3'
+		},
+		KeyA: {
+			audio: A,
+			name: 'Heater 4'
+		},
+		KeyS: {
+			audio: S,
+			name: 'Clap'
+		},
+		KeyD: {
+			audio: D,
+			name: 'Open HH'
+		},
+		KeyZ: {
+			audio: Z,
+			name: 'Kick n\' Hat'
+		},
+		KeyX: {
+			audio: X,
+			name: 'Kick'
+		},
+		KeyC: {
+			audio: C,
+			name: 'Closed HH'
+		}
+	}
+
+	useEffect(() => {
+		document.addEventListener('keydown', handleKeyDown);
+		return () => {
+			document.removeEventListener('keydown', handleKeyDown);
+		}
+	});
+
+	const handleKeyDown = (e) => {
+		playDrum(e.code);
+	}
+	const handleDrumClick = (e) => {
+		playDrum(e.target.name);
+	}
+	const playDrum = (name) => {
+		const drum = drumMap[name];
+		if (drum) {
+			setCurrentDrum(drum.name);
+			drum.audio.current.play();
+		}
+	}
+
+	return (
+		<div id="drum-machine">
+			<Display drumName={currentDrum} />
+			<div className="drum-pads">
+				<button className="drum-pad" id="drum-pad-Q" name="KeyQ" onClick={handleDrumClick}>
+					Q
+					<audio className="clip" id="Q" ref={Q} src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" />
+				</button>
+				<button className="drum-pad" id="drum-pad-W" name="KeyW" onClick={handleDrumClick}>
+					W
+					<audio className="clip" id="W" ref={W} src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" />
+				</button>
+				<button className="drum-pad" id="drum-pad-E" name="KeyE" onClick={handleDrumClick}>
+					E
+					<audio className="clip" id="E" ref={E} src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" />
+				</button>
+				<button className="drum-pad" id="drum-pad-A" name="KeyA" onClick={handleDrumClick}>
+					A
+					<audio className="clip" id="A" ref={A} src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" />
+				</button>
+				<button className="drum-pad" id="drum-pad-S" name="KeyS" onClick={handleDrumClick}>
+					S
+					<audio className="clip" id="S" ref={S} src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" />
+				</button>
+				<button className="drum-pad" id="drum-pad-D" name="KeyD" onClick={handleDrumClick}>
+					D
+					<audio className="clip" id="D" ref={D} src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" />
+				</button>
+				<button className="drum-pad" id="drum-pad-Z" name="KeyZ" onClick={handleDrumClick}>
+					Z
+					<audio className="clip" id="Z" ref={Z} src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" />
+				</button>
+				<button className="drum-pad" id="drum-pad-X" name="KeyX" onClick={handleDrumClick}>
+					X
+					<audio className="clip" id="X" ref={X} src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" />
+				</button>
+				<button className="drum-pad" id="drum-pad-C" name="KeyC" onClick={handleDrumClick}>
+					C
+					<audio className="clip" id="C" ref={C} src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" />
+				</button>
+			</div>
+		</div>
+	);
+}
+
+ReactDOM.render(<DrumMachine />, document.getElementById('root'));
+
 serviceWorker.unregister();
